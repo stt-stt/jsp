e169957eec6fbe3a2f450986de989b9ca7ec62b0
stt-stt <1208069228@qq.com>
2019-12-03 11:22:38 +0800

    my first

---
+++ src/haha/first_git.java
@@ -0,0 +1,10 @@
+package haha;
+
+public class first_git {
+	public first_git() {
+		// TODO Auto-generated constructor stub
+	}
+	public void print() {
+		System.out.println("my first git");
+	}
+}	
